3792a1aec92756a4d72dd387f8755c3bb83ce4f6
Parthborhade28 <borhadeparth24@gmail.com>
2026-08-20 11:42:39 +0530

    fix password reset authentication

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,38 +1,35 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:8080",
-// });
-
-// API.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// export default API;
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://employee-management-backend-1-rfi2.onrender.com",
+  baseURL:
+    "https://employee-management-backend-1-rfi2.onrender.com",
 });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    // Password reset APIs should work without login token
+    const publicAuthEndpoints = [
+      "/auth/login",
+      "/auth/register",
+      "/auth/forgot-password",
+      "/auth/verify-otp",
+      "/auth/reset-password",
+      "/auth/test-email",
+    ];
 
-  return config;
-});
+    const isPublicEndpoint =
+      publicAuthEndpoints.includes(config.url);
+
+    if (token && !isPublicEndpoint) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
